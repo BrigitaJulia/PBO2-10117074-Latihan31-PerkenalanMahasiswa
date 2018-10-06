05f6b94d8ae5a6f62d3e28e15c71756b1a5e32a2
MY PC <MY PC@HOME-UEFI1TB.local>
2018-10-07 06:55:36 +0700

    Project Brigita Julia Puspita Nai Goncalves 10117074 PBO2

---
+++ src/pbo2/pkg10117074/latihan31/perkenalanmahasiswa/Mhs.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117074.latihan31.perkenalanmahasiswa;
+
+/**
+ *
+ * @author Brigita Julia Puspita Nai Goncalves
+ */
+public class Mhs {
+    public String nim;
+    public String nama;
+    
+  public void PerkenalanDiri(String nim, String nama){
+      System.out.println("Hello Everyone");
+      System.out.println("My nim is " + nim);
+      System.out.println("My name is "+ nama);
+    }
+}
---
+++ src/pbo2/pkg10117074/latihan31/perkenalanmahasiswa/PBO210117074Latihan31PerkenalanMahasiswa.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117074.latihan31.perkenalanmahasiswa;
+import java.util.Scanner;
+/**
+ *
+ * @author Brigita Julia Puspita Nai Goncalves
+ */
+public class PBO210117074Latihan31PerkenalanMahasiswa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Mhs mhs1 = new Mhs();
+        mhs1.nim  = "10117074";
+        mhs1.nama = "Brigita Julia Puspita Nai Goncalves" + "\n";
+        mhs1.PerkenalanDiri(mhs1.nim, mhs1.nama);
+        
+        Mhs mhs2 = new Mhs();
+        mhs2.nim  = "10117080";
+        mhs2.nama = "Daffa Qinthara" + "\n";
+        mhs2.PerkenalanDiri(mhs2.nim, mhs2.nama);
+        
+        Mhs mhs3 = new Mhs();
+        mhs3.nim  = "10117077";
+        mhs3.nama = "Joseph Armando Carvalo" + "\n";
+        mhs3.PerkenalanDiri(mhs3.nim, mhs3.nama);
+
+        Mhs mhs4 = new Mhs();
+        mhs4.nim  = "10116228";
+        mhs4.nama = "Gandy Christian Situmorang" + "\n";
+        mhs4.PerkenalanDiri(mhs4.nim, mhs4.nama);
+    }
+    
+}
